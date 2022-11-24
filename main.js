39e9b98cbab32dafd561c03c141b451214b7ee26
phthartic <104261640+phthartic@users.noreply.github.com>
2022-11-24 21:09:38 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const main = document.querySelector('.main');
+const cancel = document.querySelector('.cancel');
+const msg = document.querySelector('#msg');
+
+while (true) {
+    let n = prompt("Enter your name");
+    
+    if(n == null) {
+        main.style.display = "none";
+        cancel.style.display = "flex";
+        msg.innerHTML = "Tayaw mo edi 'wag";
+    }
+
+    if (n.length > 0) {
+        alert("happy birthday catherine!");
+        alert('sana maging masaya ka today');
+        alert('salamat sa lahat ' + n + '!');
+        main.style.display = 'block';
+        break;
+    }
+}
